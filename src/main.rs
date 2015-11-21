62990b7b7ca72c07b89e7cabe0fa52af670726a3
Joonas Koivunen <joonas.koivunen@gmail.com>
2015-11-21 17:16:25 +0200

    main: add comments, reorder around and so on

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::io::BufReader;
 use std::fs::File;
 
 fn main() {
+    // whenever you see a .unwrap() it means the value it's
+    // being called on is an Option<?>; if the values was missing
+    // or None, the process will exit.
+
     let training_path = std::env::args().nth(1).unwrap();
     let validation_path = std::env::args().nth(2).unwrap();
 
@@ -14,7 +18,6 @@ fn main() {
 
     for ex in validation_examples {
         let classification = classify(&training_examples, &ex.pixels);
-        println!("{} classified as {}", ex.label, classification);
         if classification == ex.label {
             correct = correct + 1;
         } else {
@@ -22,19 +25,59 @@ fn main() {
         }
     }
 
-    println!("{} correct, {} wrong = {:.2}%", correct, wrong, 100.0 * (correct as f64) / ((correct + wrong) as f64));
+    println!(
+        "{} correct, {} wrong = {:.2}%",
+        correct,
+        wrong,
+        100.0 * (correct as f64) / ((correct + wrong) as f64));
+}
+
+struct Example {
+    label: i32,
+    pixels: Vec<i32>
+}
+
+fn read_file(path: &String) -> std::io::Result<Vec<Example>> {
+    let f = try!(File::open(path));
+
+    let reader = BufReader::new(f);
+
+    // this use of iterator might be losing any IO error
+    // by the use of filter_map
+    let ret = reader.lines()
+        .filter_map(|result| result.ok())
+        .skip(1)
+        .map(|line| {
+            let mut iter = line.split(',')
+                .map(|num| num.parse::<i32>())
+                .map(|result| result.unwrap());
+
+            // take the first value as a label
+            let label = iter.next().unwrap();
+
+            // collect the rest into a vector
+            let pixels = iter.collect::<Vec<i32>>();
+
+            Example{label: label, pixels: pixels}
+        }).collect::<Vec<Example>>();
+
+    Ok(ret)
 }
 
 fn distance(lhs: &Vec<i32>, rhs: &Vec<i32>) -> f64 {
+    assert!(lhs.len() == rhs.len());
     f64::sqrt(
-        lhs.iter().zip(rhs.iter())
-          .map(|(l, r)| { (l - r).pow(2)})
-          .fold(0, |acc, diff| acc + diff) as f64)
+        lhs.iter()
+            .zip(rhs.iter())
+            .map(|(left, right)| { (left - right).pow(2)})
+            .fold(0, |sum, diff_squared| sum + diff_squared) as f64)
 }
 
 fn classify(examples: &Vec<Example>, unknown: &Vec<i32>) -> i32 {
     // this looks horribly as because of https://github.com/rust-lang/rfcs/issues/675
     // cannot use iter().min_by(... -> f64) as f64 is only partial_ord
+    // https://crates.io/crates/ord_subset cannot help here as it only provides min/max
+
     let mut min: Option<(&Example, f64)> = None;
 
     for ex in examples.iter() {
@@ -64,27 +107,3 @@ fn classify(examples: &Vec<Example>, unknown: &Vec<i32>) -> i32 {
 }
 
 
-#[derive(Debug)]
-struct Example { label: i32, pixels: Vec<i32> }
-
-fn read_file(path: &String) -> std::io::Result<Vec<Example>> {
-    let f = try!(File::open(path));
-
-    let reader = BufReader::new(f);
-
-    let ret = reader.lines()
-        .filter_map(|result| result.ok())
-        .skip(1)
-        .map(|line| {
-            let mut iter = line.split(',')
-                .map(|num| num.parse::<i32>())
-                .map(|result| result.unwrap());
-
-            let label = iter.next().unwrap();
-
-            let pixels = iter.collect::<Vec<i32>>();
-
-            Example{label: label, pixels: pixels}
-        }).collect::<Vec<Example>>();
-    Ok(ret)
-}
